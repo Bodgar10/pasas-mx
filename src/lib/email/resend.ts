f624a7a88016aceb49f0e31e6c60f3d1c6d6894d
Bodgar Espinosa <bodgar_jair@hotmail.com>
2026-06-09 11:23:53 -0600

    fix(email): lazy-init Resend client inside sendEmail to avoid build-time env var access

--- src/lib/email/resend.ts
+++ src/lib/email/resend.ts
@@ -1,7 +1,5 @@
 import { Resend } from 'resend'
 
-const resend = new Resend(process.env.RESEND_API_KEY)
-
 export const FROM_EMAIL = 'Pasas.mx <hola@pasas.mx>'
 export const SUPPORT_EMAIL = 'soporte@pasas.mx'
 
@@ -13,6 +11,7 @@ export interface SendEmailOptions {
 }
 
 export async function sendEmail({ to, subject, html, replyTo }: SendEmailOptions) {
+  const resend = new Resend(process.env.RESEND_API_KEY)
   try {
     const { data, error } = await resend.emails.send({
       from: FROM_EMAIL,
