92dbd75e5059de4e6d00f844bdb2facf5a42a56a
Bodgar Espinosa <bodgar_jair@hotmail.com>
2026-06-12 11:14:18 -0600

    feat(analytics): add Google Analytics 4 tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,6 +35,22 @@ export default function RootLayout({
       className={`${orbitron.variable} ${nunito.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col" style={{ position: 'relative' }}>
+        {/* Google Analytics 4 */}
+        {process.env.NEXT_PUBLIC_GA4_MEASUREMENT_ID && (
+          <>
+            <Script
+              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA4_MEASUREMENT_ID}`}
+              strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">{`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GA4_MEASUREMENT_ID}');
+            `}</Script>
+          </>
+        )}
+
         <Script id="microsoft-clarity" strategy="afterInteractive">{`
           (function(c,l,a,r,i,t,y){
             c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
